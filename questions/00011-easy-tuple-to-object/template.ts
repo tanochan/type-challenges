a00353466e2a8d3f4d9fb5e5c0c38a407e2e9149
tanochan <shg18.sk1.hr10@gmail.com>
2022-06-30 23:26:46 +0900

    try 00011

--- questions/00011-easy-tuple-to-object/template.ts
+++ questions/00011-easy-tuple-to-object/template.ts
@@ -1,1 +1,4 @@
-type TupleToObject<T extends readonly any[]> = any
+// typeof array: https://typescriptbook.jp/tips/generates-type-from-array
+type TupleToObject<T extends readonly any[]> = {
+    [key in T[number]]: key
+}
