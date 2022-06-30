0ebab8821b17ded1ca0ae68648eacf243c1f13d9
tanochan <shg18.sk1.hr10@gmail.com>
2022-06-30 22:50:19 +0900

    try 00004

--- questions/00004-easy-pick/template.ts
+++ questions/00004-easy-pick/template.ts
@@ -1,1 +1,6 @@
-type MyPick<T, K> = any
+// keyof: https://typescriptbook.jp/reference/type-reuse/keyof-type-operator
+// Mapped type: https://typescriptbook.jp/reference/type-reuse/mapped-types
+
+type MyPick<T, K extends keyof T> = {
+    [k in K]: T[k]
+}
