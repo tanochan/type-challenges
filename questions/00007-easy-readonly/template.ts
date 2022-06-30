ec1baf399b0c535f9b9aa6f4088b69cf8ed3725b
tanochan <shg18.sk1.hr10@gmail.com>
2022-06-30 23:03:10 +0900

    try 00007

--- questions/00007-easy-readonly/template.ts
+++ questions/00007-easy-readonly/template.ts
@@ -1,1 +1,4 @@
-type MyReadonly<T> = any
+// readonly: https://typescriptbook.jp/reference/values-types-variables/object/readonly-vs-const
+type MyReadonly<T> = {
+    readonly [key in keyof T]: T[key]
+}
