05d27a339f527af6b5a2834ecd0f327b449b07dd
tanochan <shg18.sk1.hr10@gmail.com>
2022-07-02 15:45:11 +0900

    try 00018

--- questions/00018-easy-tuple-length/template.ts
+++ questions/00018-easy-tuple-length/template.ts
@@ -1,1 +1,2 @@
-type Length<T> = any
+// index signature: https://typescript-jp.gitbook.io/deep-dive/type-system/index-signatures#stringtonumberindekusanowotsu
+type Length<T extends readonly any[]> = T['length']
